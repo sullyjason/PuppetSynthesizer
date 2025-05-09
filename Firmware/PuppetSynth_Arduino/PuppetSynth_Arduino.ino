607876c7beb1ab83296a78c15ef3bc2145490783
Silvan Roth <silvan.j.roth@gmail.com>
2025-05-09 11:29:32 +0200

    Updated code for prototype v2 and updated schematic

--- Firmware/PuppetSynth_Arduino/PuppetSynth_Arduino.ino
+++ Firmware/PuppetSynth_Arduino/PuppetSynth_Arduino.ino
@@ -1,6 +1,15 @@
+/*
+TODO:
+- Check mapping accuracy and direction
+- make sure to reach max and min
+- connect resistors to rotary switches to turn into potentiometers
+- map rotary switches correctly
+- toggle debugging mode
+*/
+
 //toggle debug mode-----------------------------------------------------------------------------------------------------
 
-#define DEBUG 1
+#define DEBUG 0
 
 
 //libraries-------------------------------------------------------------------------------------------------------------
@@ -16,10 +25,11 @@ Adafruit_ADS1115 analogBoard2;    // Second ADS1115 at address 0x49
 
 #define OUT_MIN 0               // MIDI range minimum
 #define OUT_MAX 127             // MIDI range maximum  
-#define defineThreshHold 1      // Threshhold for checking for changes in MIDI values (reduced to 1)
+#define defineThreshHold 1      //Threshhold for checking for changes in MIDI values
 
 
-//input mappings---------------------------------------------------------------------------------------------------------
+//input leonardo
+// ---------------------------------------------------------------------------------------------------------
 
 struct InputMapping {
     int inputNumber;            // The index of the input
@@ -38,31 +48,43 @@ struct InputMapping {
 // The following values need to be modified if any changes are made to the hardware. Currently configured with 2 ADS1115s, called analogBoard1 and analogBoard2.
 // The remaining analog inputs are connected to the Arduino directly.
 
-InputMapping mappings[] = {
+InputMapping leonardo [] = {
 
 //  Number   functionName       pinNumber   readSignal  inMin, inMax    outMin, outMax  	mappedSignal    midiChannel
 // -----------------------------------------------------------------------------------------------------------------------
-    {0,      "EMPTY",            0,            0,            0,  0,            OUT_MIN, OUT_MAX,   0,                0            },
-    {1,      "MAIN_SPEED",        A2,            0,            1030, 40,        OUT_MIN, OUT_MAX,    0,                9            },
-    {2,      "MAIN_FREQ",        A1,            0,            1030, 40,        OUT_MIN, OUT_MAX,    0,                8            },
+    {0,      "EMPTY",			      0,			0,			0,  0,			  OUT_MIN, OUT_MAX,           0,				0			},
+    {1,      "FREQ_3",			    A2,			0,			0, 1023, 	    OUT_MIN, OUT_MAX,	          0,				12			},  // ---------------------------------------------------------------------------------------
+    {2,      "AMP_1",			      A5,			0,			15, 1000,	    OUT_MIN, OUT_MAX,	          0,				1			},
+    {3,     "AMP_2",			      A4,			0,			15, 1000,	    OUT_MIN, OUT_MAX,	          0,				2			},
+    {4,     "AMP_3",			      A1,			0,			15, 1000,	    OUT_MIN, OUT_MAX,	          0,				3			},
+    {5,     "EFFECT_1",		      A0,			0,			1023, 510,		OUT_MIN, OUT_MAX,	          0,				32			},
+    {6,     "EFFECT_3",		      A3,			0,			1023, 510,		OUT_MIN, OUT_MAX,	          0,				34			},
+};		
 
-    {3,      "WAVE_TYPE_1",        A0,            0,            40, 1030,        OUT_MIN, OUT_MAX,    0,                15            },
+InputMapping ads1[] = {
+    //  Number   functionName       pinNumber   readSignal  inMin, inMax    outMin, outMax  	mappedSignal    midiChannel
+    // -----------------------------------------------------------------------------------------------------------------------
+        {0,      "EFFECT_2",			  0,			0,			4400, 32700,		OUT_MIN, OUT_MAX,   0,				33},
+        {1,      "WAVE_TYPE_2",		  3,  		0,			20200, 29800,	OUT_MIN, OUT_MAX,	0,				16			},  //ads1
+        {2,      "FREQ_1",			    2,			0,			-10, 33000,		OUT_MIN, OUT_MAX,	0,				10			},  //ads1    ---------------------------------------------------------------------------------------
+        {3,      "FREQ_2",			    1,			0,			-10, 33000,		OUT_MIN, OUT_MAX,	0,				11			},  //ads1    ---------------------------------------------------------------------------------------
+};	
 
-    {4,      "WAVE_TYPE_2",        3,          0,            0, 33000,        OUT_MIN, OUT_MAX,    0,                16            },
-    {5,      "WAVE_TYPE_3",        0,            0,            0, 33000,        OUT_MIN, OUT_MAX,    0,                17            },
-    {6,      "FREQ_1",            2,            0,            0, 33000,        OUT_MIN, OUT_MAX,    0,                10            },
-    {7,      "FREQ_2",            0,            0,            0, 33000,        OUT_MIN, OUT_MAX,    0,                11            },
-    {8,      "FREQ_3",            2,            0,            0, 33000,     OUT_MIN, OUT_MAX,    0,                12            },
+InputMapping ads2[] = {
+    //  Number   functionName       pinNumber   readSignal  inMin, inMax    outMin, outMax  	mappedSignal    midiChannel
+    // -----------------------------------------------------------------------------------------------------------------------
+    {0,      "EMPTY",			          0,			0,			0,  0,			  OUT_MIN, OUT_MAX,   0,			0			  },
+    {1,      "MAIN_SPEED",		      2,			0,			0, 32000,		  OUT_MIN, OUT_MAX,	0,				9			  },  //ads2 (changed values)
+    {2,      "MAIN_FREQ",		        3,			0,			60, 32000,		  OUT_MIN, OUT_MAX,	0,				8			  },  //ads2 (changed values)
+    {3,      "WAVE_TYPE_1",		      0,			0,			20200, 29900,	OUT_MIN, OUT_MAX,	0,				15			},  //ads2
+    {4,      "WAVE_TYPE_3",		      1,			0,			20200,29900,	OUT_MIN, OUT_MAX,	0,				17			},  //ads2
+};
 
-    {9,      "AMP_1",            3,            0,            33000, 1500,    OUT_MIN, OUT_MAX,    0,                1            },
-    {10,     "AMP_2",            1,            0,            33000, 1500,    OUT_MIN, OUT_MAX,    0,                2            },
-    {11,     "AMP_3",            1,            0,            33000, 1500,    OUT_MIN, OUT_MAX,    0,                3            },
 
-
-    {12,     "EFFECT_1",        A4,            0,            1030, 40,        OUT_MIN, OUT_MAX,    0,                32            },
-    {13,     "EFFECT_2",        A5,            0,            1030, 40,        OUT_MIN, OUT_MAX,    0,                33            },
-    {14,     "EFFECT_3",        A6,            0,            1030, 40,        OUT_MIN, OUT_MAX,    0,                34            },
-};        
+// Move these to global scope so they persist between loop iterations
+int lastMappedSignalsLeonardo[7] = {0};
+int lastMappedSignalsAds1[4] = {0};
+int lastMappedSignalsAds2[5] = {0};
 
 void setup(){
     Serial.begin(9600);
@@ -88,91 +110,21 @@ void setup(){
     // Set the gain (optional, default is +/- 4.096V)
     analogBoard1.setGain(GAIN_ONE); // +/- 4.096V
     analogBoard2.setGain(GAIN_ONE); // +/- 4.096V
-  
 }
 
 void loop(){
 
-    //create temporary variables to check for changes
-    int lastMappedSignals[15];
+  ReadInputs();
+  MapInValues();
 
-    //set lastMappedSignals to current value
-    for(int i = 0; i < 15; i++){
-        lastMappedSignals[i] = mappings[i].mappedSignal;
-    }
-
-    //read the main faders:
-    mappings[1].readSignal = analogRead(mappings[1].pinNumber);
-    mappings[2].readSignal = analogRead(mappings[2].pinNumber);
-
-    //read the wavetype knobs:
-    mappings[3].readSignal = analogRead(mappings[3].pinNumber);
-    mappings[4].readSignal = analogBoard2.readADC_SingleEnded(mappings[4].pinNumber);
-    mappings[5].readSignal = analogBoard2.readADC_SingleEnded(mappings[5].pinNumber);
-
-    //read the frequency knobs:
-    mappings[6].readSignal = analogBoard1.readADC_SingleEnded(mappings[6].pinNumber);
-    mappings[7].readSignal = analogBoard1.readADC_SingleEnded(mappings[7].pinNumber);
-    mappings[8].readSignal = analogBoard2.readADC_SingleEnded(mappings[8].pinNumber);
-
-    //read the amps:
-    mappings[9].readSignal = analogBoard1.readADC_SingleEnded(mappings[9].pinNumber);
-    mappings[10].readSignal = analogBoard1.readADC_SingleEnded(mappings[10].pinNumber);
-    mappings[11].readSignal = analogBoard2.readADC_SingleEnded(mappings[11].pinNumber);
-
-    //read the effects:
-    mappings[12].readSignal = analogRead(mappings[12].pinNumber);
-    mappings[13].readSignal = analogRead(mappings[13].pinNumber);
-    mappings[14].readSignal = analogRead(mappings[14].pinNumber);
-
-    //maps signal between 0 - 127
-    for(int i = 0; i < 15; i++){
-        mappings[i].mappedSignal = abs(map(mappings[i].readSignal, mappings[i].inMin, mappings[i].inMax, mappings[i].outMin, mappings[i].outMax));
-    }
-
-    //checks if theres a difference between last and current Value - if yes send MIDI EVENT
-    for(int i = 0; i < 15; i++){ 
-        if(lastMappedSignals[i] - mappings[i].mappedSignal > defineThreshHold) {
-            if(i < 12){
-                sendMIDIControlChange(mappings[i].midiChannel,abs(mappings[i].mappedSignal));
-                #if DEBUG 
-                    Serial.print(mappings[i].functionName); Serial.print(": Pin "); Serial.print(mappings[i].pinNumber);
-                    Serial.print(" Read Signal: "); Serial.println(mappings[i].readSignal);
-                    Serial.print(" Mapped Signal: "); Serial.println(mappings[i].mappedSignal);
-                #endif
-            }
-        }
-    }
+  DetectChange(leonardo, 7, lastMappedSignalsLeonardo);
+  DetectChange(ads1, 4, lastMappedSignalsAds1);
+  Serial.println(ads1[0].readSignal);
+  DetectChange(ads2, 5, lastMappedSignalsAds2);
     
-    // Print the entire mappings table
-    #if DEBUG
-    printMappingsTable();
-    #endif
-    
-    //Ensure all Midi messages are sent
-    MidiUSB.flush();
-
-    // Add a small delay to reduce the frequency of prints
-    delay(500);
+  //Ensure all Midi messages are sent
+  MidiUSB.flush();
 }
-
-// Function to print the entire mappings table
-void printMappingsTable() {
-    Serial.println("Mappings Table:");
-    Serial.println("--------------------------------------------------");
-    Serial.println("Number | Function Name | Pin | Read Signal | Mapped Signal");
-    Serial.println("--------------------------------------------------");
-    for(int i = 0; i < 15; i++){
-        Serial.print(mappings[i].inputNumber); Serial.print("   | ");
-        Serial.print(mappings[i].functionName); Serial.print("       | ");
-        Serial.print(mappings[i].pinNumber); Serial.print("       | ");
-        Serial.print(mappings[i].readSignal); Serial.print("       | ");
-        Serial.println(mappings[i].mappedSignal);
-    }
-    Serial.println("--------------------------------------------------");
-}
-
-
 
 // -------------------- MIDI HELPER ------------------------
 
@@ -180,4 +132,107 @@ void sendMIDIControlChange(byte control, byte value) {
     // CC on Channel 1 => 0xB0
     midiEventPacket_t event = {0x0B, 0xB0, control, value};
     MidiUSB.sendMIDI(event);
+}
+
+void ReadInputs()
+{
+  // Read all inputs first, then process them
+  //read the main faders:
+  ads2[1].readSignal = analogBoard2.readADC_SingleEnded(ads2[1].pinNumber);
+  ads2[2].readSignal = analogBoard2.readADC_SingleEnded(ads2[2].pinNumber);
+
+  //read the wavetype knobs:
+  ads2[3].readSignal = analogBoard2.readADC_SingleEnded(ads2[3].pinNumber);
+  ads1[1].readSignal = analogBoard1.readADC_SingleEnded(ads1[1].pinNumber);
+  ads2[4].readSignal = analogBoard2.readADC_SingleEnded(ads2[4].pinNumber);
+
+  //read the frequency knobs:
+  ads1[2].readSignal = analogBoard1.readADC_SingleEnded(ads1[2].pinNumber);
+  ads1[3].readSignal = analogBoard1.readADC_SingleEnded(ads1[3].pinNumber);
+  leonardo[1].readSignal = analogRead(leonardo[1].pinNumber);
+
+  //read the amps:
+  leonardo[2].readSignal = analogRead(leonardo[2].pinNumber);
+  leonardo[3].readSignal = analogRead(leonardo[3].pinNumber);
+  leonardo[4].readSignal = analogRead(leonardo[4].pinNumber);
+
+  //read the effects:
+  leonardo[5].readSignal = analogRead(leonardo[5].pinNumber);
+  leonardo[6].readSignal = analogRead(leonardo[6].pinNumber);
+  ads1[0].readSignal = analogBoard1.readADC_SingleEnded(ads1[0].pinNumber);
+}
+
+void MapInValues()  //it works here
+{
+    //maps signal between inMin - inMax (not 0-127)
+
+    //LEONARDO
+    for(int i = 0; i < 7; i++){
+      if(i<5)
+      {
+        leonardo[i].mappedSignal = map(leonardo[i].readSignal, leonardo[i].inMin, leonardo[i].inMax, leonardo[i].outMin, leonardo[i].outMax);
+      }else{
+        if(leonardo[i].readSignal < 500)  //default
+        {
+          leonardo[i].mappedSignal = 0;
+        }else if(leonardo[i].readSignal > 500 && leonardo[i].readSignal < 1000)
+        {
+          leonardo[i].mappedSignal = 64;
+        }else{
+          leonardo[i].mappedSignal = 127;
+        }
+
+      }
+
+    }
+
+    //ADS 1
+    for(int i = 0; i < 4; i++){
+        if(i == 0)
+        {
+          if(ads1[i].readSignal < 5000)  //default
+          {
+            ads1[i].mappedSignal = 64;
+          }
+          else if(ads1[i].readSignal > 5000 && ads1[i].readSignal < 25000)
+          {
+            ads1[i].mappedSignal = 0;
+          }else{
+            ads1[i].mappedSignal = 127;
+          }
+          
+        }else if(i == 2 || i == 3)
+        { 
+          ads1[i].mappedSignal = -map(ads1[i].readSignal, ads1[i].inMin, ads1[i].inMax, ads1[i].outMin, ads1[i].outMax);
+        }
+        else{
+
+          ads1[i].mappedSignal = map(ads1[i].readSignal, ads1[i].inMin, ads1[i].inMax, ads1[i].outMin, ads1[i].outMax);
+          
+        }
+    }
+
+    //ADS 2
+    for(int i = 0; i < 5; i++){
+      ads2[i].mappedSignal = map(ads2[i].readSignal, ads2[i].inMin, ads2[i].inMax, ads2[i].outMin, ads2[i].outMax);
+    }
+}
+
+void DetectChange(InputMapping * array, int arrayLength, int lastArray[])
+{
+  for(int i = 0; i < arrayLength; i++){ 
+    if(abs(lastArray[i] - array[i].mappedSignal) > defineThreshHold) 
+    {
+
+      //Update last values AFTER comparison
+      lastArray[i] = array[i].mappedSignal;
+
+      //constrain mappedSignal between 0-127
+      array[i].mappedSignal = constrain(array[i].mappedSignal, 0, 127);
+
+      //Send MIDI event
+      sendMIDIControlChange(array[i].midiChannel, array[i].mappedSignal);
+
+    }
+  }
 }
